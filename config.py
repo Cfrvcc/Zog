8cc56d4d5928a65290571b44b5e673a621e1e107
Cfrvcc <44246449+Cfrvcc@users.noreply.github.com>
2024-07-15 02:56:27 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    BOT_TOKEN = "" # 🌚 get it from @botfather (telegram) by sending /newbot command.
-    API_ID =  # 🌚 Same as APP_ID. Get it from my.telegram.org.
-    API_HASH = "" # 🌚 Get it from my.telegram.org.
+    BOT_TOKEN = "7329793636:AAHzjjM4B1ffL8g7XMIeK11Oi8tMnqWroS0" # 🌚 get it from @botfather (telegram) by sending /newbot command.
+    API_ID = 2127284 # 🌚 Same as APP_ID. Get it from my.telegram.org.
+    API_HASH = "dfd91d5d791a9d5ffc4b085d44ed8495" # 🌚 Get it from my.telegram.org.
     
